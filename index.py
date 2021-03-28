6952f07dceef4a559b166e72b5eb5146f6643f9a
gunwoongPark <olympiodoros@naver.com>
2021-03-28 18:02:40 +0900

    로그인 후 진입

--- index.py
+++ index.py
@@ -3,3 +3,18 @@ import time
 
 chromedriver = './chromedriver.exe'
 driver = webdriver.Chrome(chromedriver)
+
+driver.get('https://software.naver.com/software/fontList.nhn?categoryId=I0000000')
+
+driver.find_element_by_xpath('//*[@id="gnb_login_button"]').click()
+
+driver.find_element_by_xpath('//*[@id="id"]').send_keys("olympiodoros")
+driver.find_element_by_xpath('//*[@id="pw"]').send_keys('park1561413@')
+
+driver.find_element_by_xpath('//*[@id="log.login"]').click()
+
+font_list = driver.find_element_by_xpath('//*[@id="_list"]')
+
+
+input()
+
