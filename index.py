32510757e521fc342796ad05d8218be5d4b38881
gunwoongPark <olympiodoros@naver.com>
2021-03-28 20:29:20 +0900

    함수화

--- index.py
+++ index.py
@@ -4,48 +4,72 @@ from selenium.common.exceptions import WebDriverException
 import time
 
 chromedriver = './chromedriver.exe'
-driver = webdriver.Chrome(chromedriver)
-
-driver.maximize_window()
-
-driver.get('https://software.naver.com/software/fontList.nhn?categoryId=I0000000')
-time.sleep(1)
-
-# # 로그인 절차
-driver.find_element_by_xpath('//*[@id="gnb_login_button"]').click()
-
-driver.find_element_by_xpath('//*[@id="id"]').send_keys("olympiodoros")
-driver.find_element_by_xpath('//*[@id="pw"]').send_keys('park1561413@')
-
-driver.find_element_by_xpath('//*[@id="log.login"]').click()
-input()
-
+driver = None
 a_href_list = []
 
-time.sleep(1)
+def openBrowser():
+    global driver
+    if not isinstance(driver, type(None)):
+        driver.close()
 
-# url 긁어오기
-for index in range(0, 100):
-    try:
-        target = driver.find_element_by_xpath('//*[@id="slot{}"]'.format(index))
-        a_href = target.find_element_by_tag_name("a").get_attribute("href")
-        a_href_list.append(a_href)
-    except NoSuchElementException:
-        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-        driver.implicitly_wait(1)
+    driver = webdriver.Chrome(chromedriver)
+    driver.maximize_window()
 
-# 다운로드
-for page in a_href_list:
-    try:
-        time.sleep(2)
-        driver.get(page)
-        time.sleep(2)
-        driver.find_element_by_xpath('//*[@id="_sticked_guide"]/div[1]/a').click()
-        time.sleep(0.5)
-        driver.find_element_by_xpath('//*[@id="showUseRangeLayerDownloadLink"]').click()
-        time.sleep(0.5)
-        driver.find_element_by_xpath('//*[@id="downloaderAlert"]/a[3]').click()
+    driver.get('https://software.naver.com/software/fontList.nhn?categoryId=I0000000')
+    time.sleep(1)
 
-    # 비정상적인 접근
-    except WebDriverException:
-        pass
+    # 로그인 절차
+    driver.find_element_by_xpath('//*[@id="gnb_login_button"]').click()
+
+    driver.find_element_by_xpath('//*[@id="id"]').send_keys("olympiodoros")
+    driver.find_element_by_xpath('//*[@id="pw"]').send_keys('park1561413@')
+
+    driver.find_element_by_xpath('//*[@id="log.login"]').click()
+    input()
+
+def urlCrawling():
+    time.sleep(1)
+
+    # url 긁어오기
+    for index in range(0, 100):
+        try:
+            target = driver.find_element_by_xpath('//*[@id="slot{}"]'.format(index))
+            a_href = target.find_element_by_tag_name("a").get_attribute("href")
+            a_href_list.append(a_href)
+        except NoSuchElementException:
+            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            time.sleep(1)
+
+def Download():
+    # 다운로드
+
+    while a_href_list:
+        page = a_href_list.pop(0)
+        try:
+            time.sleep(2)
+            driver.get(page)
+            time.sleep(2)
+            driver.find_element_by_xpath('//*[@id="_sticked_guide"]/div[1]/a').click()
+            time.sleep(0.5)
+            driver.find_element_by_xpath('//*[@id="showUseRangeLayerDownloadLink"]').click()
+            time.sleep(0.5)
+            driver.find_element_by_xpath('//*[@id="downloaderAlert"]/a[3]').click()
+
+        # 비정상적인 접근
+        except WebDriverException:
+            return
+
+def main():
+    openBrowser()
+    urlCrawling()
+
+    while True:
+        Download()
+        if not a_href_list:
+            break
+        openBrowser()
+
+if __name__ == "__main__":
+    main()
+
+
